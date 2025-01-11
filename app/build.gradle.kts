2920ad731006972806b5f77ae1d1247b6e6f2dfa
Elemolio <higaredaemilio@gmail.com>
2025-01-11 15:56:44 -0600

    Iniciar sesión por firebase(Aun no por google)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,6 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
     implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation ("com.google.firebase:firebase-auth:22.1.1")
+    implementation ("com.google.android.gms:play-services-auth:20.7.0")
 }
