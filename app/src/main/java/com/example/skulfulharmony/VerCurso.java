fdd73b311d89662ff75a651058b4809d53bd0feb
eveli <eviurzua2208@gmail.com>
2025-03-19 22:29:26 -0600

    (っ◔◡◔)っ ❤Escribir partituras ya se logro y otros cambios en xml viva

--- app/src/main/java/com/example/skulfulharmony/VerCurso.java
+++ app/src/main/java/com/example/skulfulharmony/VerCurso.java
@@ -1,11 +1,6 @@
 package com.example.skulfulharmony;
 
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,25 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class CrearClase extends AppCompatActivity {
-    private EditText et_respuestas, et_preguntas;
-    private Button btn_subirvideo, btn_subirarchivos;
+public class VerCurso extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        et_respuestas = findViewById(R.id.et_opcrespuesta);
-
-        setContentView(R.layout.activity_crearclase);
+        setContentView(R.layout.activity_vercurso);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-
     }
-
 }
---
+++ app/src/main/java/com/example/skulfulharmony/adapters/AdapterVerClaseHistorial.java
@@ -0,0 +1,4 @@
+package com.example.skulfulharmony.adapters;
+
+public class AdapterVerClaseHistorial {
+}
