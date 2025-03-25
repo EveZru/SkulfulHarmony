9979578f6ea48042c8f9d8bc374e73b94600bc2e
eveli <eviurzua2208@gmail.com>
2025-03-24 22:30:47 -0600

    Ya estan las preguntas en el de clases originales 	ʕ ノ • ᴥ • ʔ ノ ︵ ┻━┻

--- app/src/main/java/com/example/skulfulharmony/ClasesOriginales.java
+++ app/src/main/java/com/example/skulfulharmony/ClasesOriginales.java
@@ -7,6 +7,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.skulfulharmony.adapters.AdapterPreguntasEnClasesOriginales;
+import com.example.skulfulharmony.javaobjects.miscellaneous.questions.PreguntaCuestionario;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ClasesOriginales extends AppCompatActivity {
 
@@ -20,5 +29,23 @@ public class ClasesOriginales extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        //------------------------------------------ esto es para probar que funcionan las preguntas localmente falta que se conecten a la bd y consigan la info
+        List<PreguntaCuestionario> listaPreguntas = new ArrayList<>();
+        listaPreguntas.add(new PreguntaCuestionario("¿Cuál es la capital de Francia?",
+                new ArrayList<>(Arrays.asList("París", "Madrid", "Berlín")), 0));
+
+        listaPreguntas.add(new PreguntaCuestionario("¿Cuántos continentes hay?",
+                new ArrayList<>(Arrays.asList("4", "6")), 1));
+
+        listaPreguntas.add(new PreguntaCuestionario("¿Cuáles son colores primarios?",
+                new ArrayList<>(Arrays.asList("Rojo", "Verde", "Azul", "Amarillo")), 0));
+
+        RecyclerView recyclerView = findViewById(R.id.rv_preguntas_claseoriginal);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        AdapterPreguntasEnClasesOriginales adapter = new AdapterPreguntasEnClasesOriginales(listaPreguntas, this);
+        recyclerView.setAdapter(adapter);
+
+
+
     }
 }
