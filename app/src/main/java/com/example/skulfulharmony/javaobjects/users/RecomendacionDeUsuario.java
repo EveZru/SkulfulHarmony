534ade68f92c8e91c35da5109e993027c33642c5
ElEmolio <higaredaemilio@gamil.com>
2025-06-04 20:49:30 -0600

    Recomendaciones de usuario "borrado"

--- app/src/main/java/com/example/skulfulharmony/javaobjects/users/RecomendacionDeUsuario.java
+++ app/src/main/java/com/example/skulfulharmony/javaobjects/users/RecomendacionDeUsuario.java
@@ -1,33 +1,33 @@
-package com.example.skulfulharmony.javaobjects.users;
-
-import com.example.skulfulharmony.javaobjects.courses.Curso;
-
-import java.util.List;
-
-public class RecomendacionDeUsuario {
-
-    private List<ClusterCursos> clusterClases;
-    private RanqueadorCluster ranqueador;
-    private GestionClustering gestionClustering;  // Gestión de los clústeres
-
-
-
-    // Constructor actualizado para incluir GestionClustering
-    public RecomendacionDeUsuario(RanqueadorCluster ranqueador, GestionClustering gestionClustering) {
-        this.ranqueador = ranqueador;
-        this.gestionClustering = gestionClustering;
-    }
-
-    public List<Curso> generarRecomendaciones(Usuario usuario) {
-        // Calcular el instrumento principal del usuario
-        ranqueador.calcularInstrumentoPrincipal(usuario.getHistorialCursos());
-
-        // Obtener los cursos recomendados
-        for (ClusterCursos cluster : gestionClustering.getClusters()) {
-            if (cluster.getInstrumento().equals(ranqueador.getInstrumentoPrincipal())) {
-                return cluster.obtenerCursosRecomendados(usuario);
-            }
-        }
-        return null;  // Si no hay recomendaciones disponibles
-    }
-}
+//package com.example.skulfulharmony.javaobjects.users;
+//
+//import com.example.skulfulharmony.javaobjects.courses.Curso;
+//
+//import java.util.List;
+//
+//public class RecomendacionDeUsuario {
+//
+//    private List<ClusterCursos> clusterClases;
+//    private RanqueadorCluster ranqueador;
+//    private GestionClustering gestionClustering;  // Gestión de los clústeres
+//
+//
+//
+//    // Constructor actualizado para incluir GestionClustering
+//    public RecomendacionDeUsuario(RanqueadorCluster ranqueador, GestionClustering gestionClustering) {
+//        this.ranqueador = ranqueador;
+//        this.gestionClustering = gestionClustering;
+//    }
+//
+//    public List<Curso> generarRecomendaciones(Usuario usuario) {
+//        // Calcular el instrumento principal del usuario
+//        ranqueador.calcularInstrumentoPrincipal(usuario.getHistorialCursos());
+//
+//        // Obtener los cursos recomendados
+//        for (ClusterCursos cluster : gestionClustering.getClusters()) {
+//            if (cluster.getInstrumento().equals(ranqueador.getInstrumentoPrincipal())) {
+//                return cluster.obtenerCursosRecomendados(usuario);
+//            }
+//        }
+//        return null;  // Si no hay recomendaciones disponibles
+//    }
+//}
